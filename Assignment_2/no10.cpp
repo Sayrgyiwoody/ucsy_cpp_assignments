fab6265c480a951ef23e59211bcd9cd079c5032b
Mg Waiyan Tun <115869082+Sayrgyiwoody@users.noreply.github.com>
2023-03-01 19:54:11 +0630

    Update no10.cpp

--- Assignment_2/no10.cpp
+++ Assignment_2/no10.cpp
@@ -3,23 +3,35 @@ using namespace std;
 
 int main() {
     int a,b,c,d;
-    char slash , calculate;
-    cout << "Do you want to continue (y/n) = ";
-    cin >> calculate;
-    while (calculate == 'y') {
-        cout << "Enter first number = " ;
-        cin >> a >> slash >> b ;
-        cout << "Enter second number = ";
-        cin >> c >> slash >> d ;
-
-        cout << "Addition : " << (a*d + b*c) << slash << (b*d) << endl;
-        cout << "Subtraction : " << (a*d - b*c) << slash << (b*d) << endl;
-        cout << "Addition : " << (a*c) << slash << (b*d) << endl;
-        cout << "Addition : " << (a*d) << slash << (b*c) << endl;
-    
-    cout << "Do you want to continue (y/n) = ";
-    cin >> calculate;
+    char slash,choice,_operator;
+    cout << "Do you want to continue: ";
+    cin >> choice ;
+    while (choice == 'y') {
+        cout << "Enter first fraction : ";
+        cin >> a >> slash >> b;
+        cout << "Enter operator : ";
+        cin >> _operator;
+        cout << "Enter second fraction : ";
+        cin >> c >> slash >> d;
+        switch (_operator) {
+            case '+':
+            cout << "Answer : " << a*d + b*c << slash << b*d << endl;
+            break;
+            case '-':
+            cout << "Answer : " << a*d - b*c << slash << b*d << endl;
+            break;
+            case '*':
+            cout << "Answer : " << a*c << slash << b*d <<endl;
+            break;
+            case '/':
+            cout << "Answer : " << a*d << slash << b*c << endl;
+            break;
+            default:
+            cout << "Wrong Operator!";
+            break;
+        }
+        cout << "Do you want to continue: ";
+        cin >> choice;
     }
-    
     return 0;
 }
