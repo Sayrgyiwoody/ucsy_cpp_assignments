fdf60c87a840aa9802fbef49832e3b1d72b43ef6
Mg Waiyan Tun <115869082+Sayrgyiwoody@users.noreply.github.com>
2023-03-01 15:02:16 +0630

    Update no2.cpp

--- Assignment_1/no2.cpp
+++ Assignment_1/no2.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     const int num1 = 10;
-    int num2 = num1 + 10;
-    int num3 = --num2;
-
-    cout << num1 << " " << num2 << " " << num3 << endl;
-
+    int num2 = num1;
+    num2+=10;
+    cout << num1 << " " << num2 << " " << --num2;
     return 0;
 }
